d83bd9fe5263a54559cc42e8b038b954fd49c66f
Ai Akata <202305-AP-01-ai-akata>
2023-07-21 13:46:06 +0900

    redmineチケット#14174
    【状況】仕入更新画面へ遷移した時、距離がすべて「A」で表示される
    【修正後】登録した距離が表示される

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -242,7 +242,7 @@ function subStockEditView($param)
       <tr>
         <th>除外</th>
         <td><input type="radio" name="del" value="1" checked="1" /> 非除外
-          <input type="radio" name="del" value="0" /> 除外
+          <input type="radio" name="del" value="0" <?php if ($param["del"] === "0") print ' checked="1"'; ?> /> 除外
         </td>
       </tr>
       <tr>
@@ -287,7 +287,9 @@ function subStockEditView($param)
           <?php
           for ($i = 0; $i < 4; $i++) {
           ?>
-            <input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if (1 == $i + 1) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
+            <input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if (1 == $i + 1) {
+                                                                                  print ' checked="checked"';
+                                                                                } elseif ($param["distance"] == $i + 1) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
           <?php
           }
           ?>
